b01a4372dfcfd487952d88e18e86b92335275244
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-18 15:20:00 +0530

    a

---
+++ src/components/Cities.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ListTown from "./Towns";
+
+const ListCity = (props) => {
+  const [ShowTown, setShowTown] = useState(false);
+  return (
+    <>
+      <li onClick={() => setShowTown(!ShowTown)} id={"city" + (props.id + 1)}>
+        {props.CityName.name}
+      </li>
+      {ShowTown ? (
+        <ul>
+          {props.CityName.towns.map((Townname, TownIndex) => {
+            return (
+              <ListTown TownName={Townname} key={TownIndex} id={TownIndex} />
+            );
+          })}
+        </ul>
+      ) : null}
+    </>
+  );
+};
+export default ListCity;
---
+++ src/components/Towns.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ListTown = (props) => {
+  return <li id={"town" + (props.id + 1)}>{props.TownName.name}</li>;
+};
+export default ListTown;
